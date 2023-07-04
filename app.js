9bf5a3d929c13f5a32100fe958c4dd54add9cbf7
Jashanpreet20 <jashankot20@gmail.com>
2023-07-04 08:12:56 +0530

    sign up page

---
+++ app.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const bodyparser=require('body-parser');
+const cors=require('cors');
+const app=express();
+
+app.use(bodyparser.json());
+app.use(cors());
+
+
+app.listen(3000,() =>{
+    console.log('server run at 3000');
+})
